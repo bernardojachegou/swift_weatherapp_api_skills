285401f1f943c06c722dd5dc991fe03ea6f071dc
Michel Bernardo <michel.bernardo@db1.com.br>
2022-04-08 15:06:49 -0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Michel Franklin Silva Bernardo on 08/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
